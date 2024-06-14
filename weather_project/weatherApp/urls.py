3fc3d58a914f679926d1a0bf845e150bcfca10f7
saurabh tadge <sayalitadge07@gmail.com>
2024-06-14 22:40:08 +0530

    created project and app, initial configuration

---
+++ weather_project/weatherApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  # Import views from the same directory
+
+urlpatterns = [
+    # path('', views.index, name='index'),  # Example URL pattern
+    # Add other URL patterns here
+]
